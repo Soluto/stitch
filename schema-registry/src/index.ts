eea4a520bc61f218b71b63fb169996dc76abe9f2
Zahi Cohen <zahi@zahi-mac.soluto.local>
2019-02-24 15:36:43 +0200

    Working server

--- schema-registry/src/index.ts
+++ schema-registry/src/index.ts
@@ -1,10 +1,11 @@
-console.log("starting");
+import * as express from "express";
 
-type x = {};
+const PORT = process.env.PORT || 4000;
 
-setInterval(async () => {
-  console.log(".");
-  console.log("*");
-  console.log("!");
-  console.log("#");
-}, 1000);
+const app = express();
+
+app.use("/isAlive", async (_: any, res: any) => res.send(true));
+
+app.listen({ port: PORT }, () =>
+  console.log(`🚀 Server ready at http://localhost:${PORT}`)
+);
