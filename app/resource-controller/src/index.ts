c8c8136dc11c0f571c3377a871ee86892e5fc7d5
Zahi Cohen <zahi@zahi-mac.soluto.local>
2019-02-24 14:10:04 +0200

    Added debug option for node service

--- app/resource-controller/src/index.ts
+++
@@ -1,4 +0,0 @@
-console.log('starting');
-
-setTimeout(function () {
-}, 200000);
---
+++ resource-controller/index.ts
@@ -0,0 +1,10 @@
+console.log("starting");
+
+type x = {};
+
+setInterval(async () => {
+  console.log(".");
+  console.log("*");
+  console.log("!");
+  console.log("#");
+}, 1000);
