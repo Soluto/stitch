46fa74a2ced3f8ecd56463f6607e67b59b2fdec1
yoavkarako <yoav@soluto.com>
2019-03-20 14:01:01 +0200

    Http directive - gateway side (#20)

--- schema-registry/src/sources/file/index.ts
+++ schema-registry/src/sources/file/index.ts
@@ -1,44 +1,45 @@
 import * as fs from "fs";
 import Source from "..";
+import * as util from "util";
+import * as glob from "glob"
 
-const util = require("util");
-const glob = util.promisify(require("glob"));
+const globAsync = util.promisify(glob);
 
 const readFileAsync = util.promisify(fs.readFile);
 
 type NamedGqlSchema = {
-  name: string;
-  gqlSchema: string;
+    name: string;
+    gqlSchema: string;
 };
 
 const remoteSource: Source = {
-  async getSchemas() {
-    const files = await glob("./*.gql", {});
-    const getSchemaFromFiles: Promise<NamedGqlSchema>[] = files.map(
-      async (file: string) => {
-        const fileNameMatch = file.match(/([^\/]+)(?=\.\w+$)/);
-        if (!fileNameMatch) throw "error extracting filename";
+    async getSchemas() {
+        const files = await globAsync("**/*.gql", {});
+        const getSchemaFromFiles: Promise<NamedGqlSchema>[] = files.map(
+            async (file: string) => {
+                const fileNameMatch = file.match(/([^\/]+)(?=\.\w+$)/);
+                if (!fileNameMatch) throw "error extracting filename";
 
-        const [name] = fileNameMatch;
-        const fileBuffer = await readFileAsync(file, "utf8");
-        return { name: name, gqlSchema: fileBuffer };
-      }
-    );
+                const [name] = fileNameMatch;
+                const fileBuffer = await readFileAsync(file, "utf8");
+                return { name: name, gqlSchema: fileBuffer };
+            }
+        );
 
-    const gqlByNames = await Promise.all(getSchemaFromFiles);
+        const gqlByNames = await Promise.all(getSchemaFromFiles);
 
-    return gqlByNames.reduce(
-      (agg, { name, gqlSchema }) => ({
-        ...agg,
-        [name]: gqlSchema
-      }),
-      {}
-    );
-  },
+        return gqlByNames.reduce(
+            (agg, { name, gqlSchema }) => ({
+                ...agg,
+                [name]: gqlSchema
+            }),
+            {}
+        );
+    },
 
-  async registerSchema(name: string, gqlSchema: string) {
-    await fs.writeFileSync(`./${name}.gql`, gqlSchema);
-  }
+    async registerSchema(name: string, gqlSchema: string) {
+        await fs.writeFileSync(`./${name}.gql`, gqlSchema);
+    }
 };
 
 export default remoteSource;
