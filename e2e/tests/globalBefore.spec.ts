c2d750988e1afbcd20db3725c59c8afb0afe3c02
AleF83 <alexanderfux83@gmail.com>
2019-07-08 13:13:45 +0300

    ...

--- e2e/tests/globalBefore.spec.ts
+++ e2e/tests/globalBefore.spec.ts
@@ -1,22 +1,24 @@
-import waitOn = require('wait-on');
+// import waitOn = require('wait-on');
 import { URL } from 'url';
+import * as bluebird from 'bluebird';
 
 before('waiting for graphql server', async function () {
     this.timeout(60000);
-    const { hostname, port, protocol } = new URL(process.env.GRAPHQL_SERVER_URL);
+    // const { hostname, port, protocol } = new URL(process.env.GRAPHQL_SERVER_URL);
 
-    const explicitPort = port || getImplicitPort(protocol);
-    await waitOn({
-        log: true,
-        verbose: true,
-        resources: [
-            `tcp:${hostname}:${explicitPort}`,
-        ],
-        delay: 1000,
-        interval: 100,
-        timeout: 60000,
-        window: 1000,
-    });
+    // const explicitPort = port || getImplicitPort(protocol);
+    // await waitOn({
+    //     log: true,
+    //     verbose: true,
+    //     resources: [
+    //         `tcp:${hostname}:${explicitPort}`,
+    //     ],
+    //     delay: 1000,
+    //     interval: 100,
+    //     timeout: 60000,
+    //     window: 1000,
+    // });
+    await bluebird.delay(20000);
 });
 
 const getImplicitPort = protocol => {
