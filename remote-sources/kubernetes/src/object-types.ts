859565a136a93cb8580607ba633ca342442d537a
Aviv Rozenboim <aviv@soluto.com>
2019-07-16 17:37:14 +0300

    Enriching based on custom resource's namespace, not from a predefined "agogos" namespace

--- remote-sources/kubernetes/src/object-types.ts
+++ remote-sources/kubernetes/src/object-types.ts
@@ -1,37 +1,40 @@
+import k8s = require('@kubernetes/client-node');
+
 export type SchemaConfig = {
-  definition: string;
+    definition: string;
 };
 
 export type UpstreamConfig = {
-  host: string;
-  headers: {
-    name: string;
-    value: string;
-  }[];
-  auth: {
-    type: string;
-    authority: string;
-    scope: string;
-  };
+    host: string;
+    headers: {
+        name: string;
+        value: string;
+    }[];
+    auth: {
+        type: string;
+        authority: string;
+        scope: string;
+    };
 };
 
 export type UpstreamClientCredentialsConfig = {
-  type: string;
-  authority: string;
-  clientId: string;
-  clientSecret:
-    | string
-    | {
-        valueFrom: {
-          secretKeyRef: {
-            secretName: string;
-            key: string;
+    type: string;
+    authority: string;
+    clientId: string;
+    clientSecret:
+        | string
+        | {
+              valueFrom: {
+                  secretRef: {
+                      secretName: string;
+                      key: string;
+                  };
+              };
           };
-        };
-      };
 };
 
-export type AgogosObjectConfig =
-  | SchemaConfig
-  | UpstreamConfig
-  | UpstreamClientCredentialsConfig;
+export type AgogosObjectConfig = SchemaConfig | UpstreamConfig | UpstreamClientCredentialsConfig;
+
+export type AgogosCustomResource<T extends AgogosObjectConfig = AgogosObjectConfig> = k8s.KubernetesObject & {
+    spec: T;
+};
