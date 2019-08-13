e553f127d208b74f14afcbc3b9a69f4d8bf45271
Aviv Rozenboim <aviv@soluto.com>
2019-08-13 17:34:17 +0300

    Renamed alias(from)->from(field)

--- services/graphql-server/directives/common/from.go
+++ services/graphql-server/directives/common/from.go
@@ -9,13 +9,13 @@ import (
 	"github.com/vektah/gqlparser/ast"
 )
 
-var aliasMiddleware = middlewares.DirectiveDefinition{
+var fromMiddleware = middlewares.DirectiveDefinition{
 	MiddlewareFactory: func(f *ast.FieldDefinition, d *ast.Directive) middlewares.Middleware {
 		args := d.ArgumentMap(make(map[string]interface{}))
-		originalFieldName, ok := args["from"].(string)
+		originalFieldName, ok := args["field"].(string)
 
 		if !ok {
-			logrus.Panic("'from' argument missing or not a string in @alias directive")
+			logrus.Panic("'field' argument missing or not a string in @from directive")
 		}
 
 		return middlewares.Leaf(func(rp graphql.ResolveParams) (interface{}, error) {
--- services/graphql-server/directives/common/index.go
+++ services/graphql-server/directives/common/index.go
@@ -3,15 +3,11 @@ package common
 import "agogos/directives/middlewares"
 
 // MiddlewareDefinitions common
-var MiddlewareDefinitions = getMiddlewareDefinitions()
-
-func getMiddlewareDefinitions() map[string]middlewares.MiddlewareDefinition {
-	directives := make(map[string]middlewares.MiddlewareDefinition)
-	directives["log"] = log
-	directives["overrideContext"] = overrideContext
-	directives["stub"] = stub
-	directives["http"] = httpMiddleware
-	directives["select"] = selectMiddleware
-	directives["alias"] = aliasMiddleware
-	return directives
+var MiddlewareDefinitions = map[string]middlewares.MiddlewareDefinition{
+	"log":             log,
+	"overrideContext": overrideContext,
+	"stub":            stub,
+	"http":            httpMiddleware,
+	"select":          selectMiddleware,
+	"from":            fromMiddleware,
 }
