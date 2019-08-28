4d1e4b92ce766537adf44c301b5084b382e0e575
Aviv Rozenboim <aviv@soluto.com>
2019-08-28 17:22:51 +0300

    Removed `log` and `overrideContext` directives

--- services/graphql-server/directives/common/index.go
+++ services/graphql-server/directives/common/index.go
@@ -4,9 +4,7 @@ import "agogos/directives/middlewares"
 
 // MiddlewareDefinitions common
 var MiddlewareDefinitions = map[string]middlewares.MiddlewareDefinition{
-	"log":             log,
-	"overrideContext": overrideContext,
-	"stub":            stub,
-	"http":            httpMiddleware,
-	"select":          selectMiddleware,
+	"stub":   stub,
+	"http":   httpMiddleware,
+	"select": selectMiddleware,
 }
