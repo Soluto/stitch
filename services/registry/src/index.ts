1df197ddb4428889f271a6664622a89289dee2d8
AleF83 <alexanderfux83@gmail.com>
2019-09-26 16:28:33 +0300

    registy eslint run

--- services/registry/src/index.ts
+++ services/registry/src/index.ts
@@ -1,11 +1,11 @@
-import nconf = require("nconf");
-import logger, { loggingMiddleware } from "./logger";
-nconf.env("__");
+import logger, { loggingMiddleware } from './logger';
 
-import * as express from "express";
-import gqlSchemaRoute from "./routes/schema";
-import validateRoute from "./routes/validate";
-import { startGrpcServer } from "./sync/sync-grpc-server";
+import * as express from 'express';
+import gqlSchemaRoute from './routes/schema';
+import validateRoute from './routes/validate';
+import { startGrpcServer } from './sync/sync-grpc-server';
+import nconf = require('nconf');
+nconf.env('__');
 
 const PORT = process.env.PORT || 4000;
 
@@ -13,18 +13,18 @@ const app = express();
 
 app.use(loggingMiddleware);
 
-app.get("/health", (_: express.Request, res: express.Response) =>
-    res.send(true),
+app.get('/health', (_: express.Request, res: express.Response) =>
+  res.send(true)
 );
-app.get("/metrics", (_: express.Request, res: express.Response) =>
-    res.send(true),
+app.get('/metrics', (_: express.Request, res: express.Response) =>
+  res.send(true)
 );
 
-app.use("/schema", gqlSchemaRoute);
-app.use("/validate", validateRoute);
+app.use('/schema', gqlSchemaRoute);
+app.use('/validate', validateRoute);
 
 app.listen({ port: PORT }, () =>
-    logger.info(`🚀 HTTP Server ready at http://localhost:${PORT}`),
+  logger.info(`🚀 HTTP Server ready at http://localhost:${PORT}`)
 );
 
 startGrpcServer();
--- services/registry/src/sources-config.ts
+++ services/registry/src/sources-config.ts
@@ -1,21 +1,21 @@
-import nconf = require("nconf");
-import * as R from "ramda";
-import Source, { remoteSource } from "./sources";
-import createFileSource from "./sources/file";
+import * as R from 'ramda';
+import Source, { remoteSource } from './sources';
+import createFileSource from './sources/file';
+import nconf = require('nconf');
 
 const remoteSources: { [source: string]: Source } = R.map(
-    url => remoteSource(url),
-    nconf.get("REMOTESOURCE") || {}
+  url => remoteSource(url),
+  nconf.get('REMOTESOURCE') || {}
 );
 
 // TODO: check relative and absolute paths
-const BUILTIN_RESOURCES_FOLDER = "./builtin-resources";
+const BUILTIN_RESOURCES_FOLDER = './builtin-resources';
 
 const sources: { [source: string]: Source } = {
-    builtin: createFileSource(BUILTIN_RESOURCES_FOLDER),
-    ...remoteSources
+  builtin: createFileSource(BUILTIN_RESOURCES_FOLDER),
+  ...remoteSources
 };
 
 export default sources;
 
-export const defaultSource = "builtin";
+export const defaultSource = 'builtin';
