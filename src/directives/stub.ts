5b2ff0f9a8599b669b82cfe65aba737117848c3e
Aviv Rozenboim <aviv@soluto.com>
2020-01-12 15:19:16 +0200

    Stub supports param injection

--- src/directives/stub.ts
+++ src/directives/stub.ts
@@ -1,12 +1,13 @@
 import {SchemaDirectiveVisitor} from 'graphql-tools';
 import {GraphQLField} from 'graphql';
 import {gql} from 'apollo-server-core';
+import {injectParameters} from '../param-injection';
 
 export class StubDirective extends SchemaDirectiveVisitor {
     visitFieldDefinition(field: GraphQLField<any, any>) {
         const {value} = this.args;
 
-        field.resolve = () => value;
+        field.resolve = (parent, args, context) => injectParameters(value, parent, args, context);
     }
 }
 
