e97061cc51e1d3331737bd202b8e3106b0793db5
Aviv Rozenboim <aviv@soluto.com>
2020-01-12 11:47:47 +0200

    Added operationType to gql directive

--- src/directives/gql.ts
+++ src/directives/gql.ts
@@ -7,14 +7,15 @@ import {injectParametersToObject} from '../param-injection';
 
 export class GqlDirective extends SchemaDirectiveVisitor {
     visitFieldDefinition(field: GraphQLField<any, any>) {
-        const {url, fieldName, arguments: gqlArgs} = this.args;
+        const {url, fieldName, arguments: gqlArgs, operationType: operationTypeParam} = this.args;
+        const operationType = operationTypeParam?.toLowerCase() ?? 'query';
         const link = new HttpLink({uri: url, fetch: fetch as any});
         const remoteSchema = introspectSchema(link).then(schema => makeRemoteExecutableSchema({schema, link}));
 
         field.resolve = async (parent, args, context, info) => {
             return await delegateToSchema({
                 schema: await remoteSchema, // TODO: Take this "await" outside of the hot path somehow
-                operation: 'query',
+                operation: operationType,
                 fieldName,
                 args: injectParametersToObject(gqlArgs, parent, args, context),
                 context,
@@ -25,5 +26,15 @@ export class GqlDirective extends SchemaDirectiveVisitor {
 }
 
 export const sdl = gql`
-    directive @gql(url: String!, fieldName: String!, arguments: JSONObject) on FIELD_DEFINITION
+    enum GraphQLOperationType {
+        Query
+        Mutation
+    }
+
+    directive @gql(
+        url: String!
+        fieldName: String!
+        operationType: GraphQLOperationType
+        arguments: JSONObject
+    ) on FIELD_DEFINITION
 `;
