768fd76822971d29b94c2b2e76628e8e1e6ec842
Aviv Rozenboim <aviv@soluto.com>
2020-01-21 17:44:28 +0200

    Removed federation execution model, replaced with standard graphql execution model

--- src/modules/stitchGateway.ts
+++ src/modules/stitchGateway.ts
@@ -1,65 +1,80 @@
-import {ApolloGateway, LocalGraphQLDataSource, ServiceEndpointDefinition} from '@apollo/gateway';
-import {
-    gql,
-    GraphQLService,
-    GraphQLServiceEngineConfig,
-    Unsubscriber,
-    GraphQLServiceConfig,
-    SchemaChangeCallback,
-} from 'apollo-server-core';
+import {Unsubscriber, GraphQLServiceConfig, SchemaChangeCallback} from 'apollo-server-core';
+import {parse, execute} from 'graphql';
+import {Observable, Subscription} from 'rxjs';
+import {shareReplay, map, take} from 'rxjs/operators';
+
 import {directiveMap} from './directives';
 import {ResourceGroup, UpstreamClientCredentials, Upstream} from './resource-repository';
-import {buildFederatedSchemaDirectivesHack} from './buildFederatedSchema';
+import {buildSchemaFromFederatedTypeDefs} from './buildFederatedSchema';
 import * as baseSchema from './baseSchema';
 import {ActiveDirectoryAuth} from './activeDirectoryAuth';
+import {sdl as directivesSdl} from './directives';
 
-export function createApolloGateway(rg: ResourceGroup): GraphQLService {
-    const schemas = Object.fromEntries(
-        rg.schemas.map(schema => {
-            const name = `${schema.metadata.namespace}.${schema.metadata.name}`;
-            return [name, {schema: schema.schema, url: `http://graph/${name}`, name}];
+export function createApolloGateway(config: {resourceGroups: Observable<ResourceGroup>}) {
+    let currentSchemaConfig: GraphQLServiceConfig | null = null;
+
+    const subscription = new Subscription();
+    const resourceGroupsObs = config.resourceGroups.pipe(map(createSchemaConfig), shareReplay(1));
+    subscription.add(
+        resourceGroupsObs.subscribe(schemaConfig => {
+            console.log('New schema loaded');
+            currentSchemaConfig = schemaConfig;
         })
     );
+
+    return {
+        async load(): Promise<GraphQLServiceConfig> {
+            currentSchemaConfig = await resourceGroupsObs.pipe(take(1)).toPromise();
+
+            return {
+                schema: currentSchemaConfig.schema,
+                executor(requestContext) {
+                    return currentSchemaConfig!.executor(requestContext);
+                },
+            };
+        },
+        onSchemaChange(callback: SchemaChangeCallback): Unsubscriber {
+            const sub = resourceGroupsObs.pipe(map(sc => sc.schema)).subscribe(callback);
+
+            return sub.unsubscribe.bind(sub);
+        },
+        dispose() {
+            subscription.unsubscribe();
+        },
+    };
+}
+
+function createSchemaConfig(rg: ResourceGroup): GraphQLServiceConfig {
     const activeDirectoryAuth = new ActiveDirectoryAuth();
     const upstreamsByHost = new Map(rg.upstreams.map(u => [u.host, u]));
     const upstreamClientCredentialsByAuthority = new Map(
         rg.upstreamClientCredentials.map(u => [u.activeDirectory.authority, u])
     );
 
-    const gateway = new ApolloGateway({
-        serviceList: Object.values(schemas),
-        buildService: (definition: ServiceEndpointDefinition) => {
-            const typeDef = schemas[definition.name].schema;
-            const typeDefWithBase = gql`
-                ${baseSchema.typeDef}
-                ${typeDef}
-            `;
-            const schema = buildFederatedSchemaDirectivesHack({
-                typeDef: typeDefWithBase,
-                resolvers: baseSchema.resolvers,
-                schemaDirectives: directiveMap,
-            });
-            return new LocalGraphQLDataSource(schema);
-        },
+    const schema = buildSchemaFromFederatedTypeDefs({
+        typeDefs: Object.fromEntries(
+            rg.schemas.map(s => [`${s.metadata.namespace}/${s.metadata.name}`, parse(s.schema)])
+        ),
+        baseTypeDefs: baseSchema.baseTypeDef,
+        directiveTypeDefs: directivesSdl,
+        resolvers: baseSchema.resolvers,
+        schemaDirectives: directiveMap,
     });
 
     return {
-        async load(options: {engine?: GraphQLServiceEngineConfig}): Promise<GraphQLServiceConfig> {
-            const config = await gateway.load(options);
+        schema,
+        executor(requestContext) {
+            requestContext.context.upstreams = upstreamsByHost;
+            requestContext.context.upstreamClientCredentials = upstreamClientCredentialsByAuthority;
+            requestContext.context.activeDirectoryAuth = activeDirectoryAuth;
 
-            return {
-                ...config,
-                executor(requestContext) {
-                    requestContext.context.upstreams = upstreamsByHost;
-                    requestContext.context.upstreamClientCredentials = upstreamClientCredentialsByAuthority;
-                    requestContext.context.activeDirectoryAuth = activeDirectoryAuth;
-
-                    return config.executor(requestContext);
-                },
-            };
-        },
-        onSchemaChange(callback: SchemaChangeCallback): Unsubscriber {
-            return gateway.onSchemaChange(callback);
+            return execute({
+                document: requestContext.document,
+                schema: schema,
+                contextValue: requestContext.context,
+                operationName: requestContext.operationName,
+                variableValues: requestContext.request.variables,
+            });
         },
     };
 }
