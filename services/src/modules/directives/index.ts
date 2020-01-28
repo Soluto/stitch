82706d8cdc8b396095040665ba41c8750417ad3c
Aviv Rozenboim <aviv@soluto.com>
2020-01-28 14:57:09 +0200

    @select directive to support existing usages, can be removed later on

--- services/src/modules/directives/index.ts
+++ services/src/modules/directives/index.ts
@@ -4,12 +4,14 @@ import {sdl as stubSdl, StubDirective} from './stub';
 import {sdl as restSdl, RestDirective} from './rest';
 import {sdl as gqlSdl, GqlDirective} from './gql';
 import {sdl as exportSdl, ExportDirective} from './export';
+import {sdl as selectSdl, SelectDirective} from './select';
 
 export const directiveMap: {[visitorName: string]: typeof SchemaDirectiveVisitor} = {
     stub: StubDirective,
     rest: RestDirective,
     gql: GqlDirective,
     export: ExportDirective,
+    select: SelectDirective,
 };
 
-export const sdl = concatAST([stubSdl, restSdl, gqlSdl, exportSdl]);
+export const sdl = concatAST([stubSdl, restSdl, gqlSdl, exportSdl, selectSdl]);
