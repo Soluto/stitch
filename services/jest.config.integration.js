ed354ed1d5b438feb0f5f1a404a5cec78bc6ff63
Aviv Rozenboim <aviv@soluto.com>
2020-03-04 14:34:20 +0200

    Restructured tests

--- services/jest.config.integration.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    roots: ['<rootDir>/src/integration'],
-    setupFiles: ['./setupJestIntegration.js'],
-    testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-    transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-};
--- services/jest.config.js
+++ services/jest.config.js
@@ -1,9 +1,25 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    testPathIgnorePatterns: ['/node_modules/', '/src/integration'],
-    setupFiles: ['./setupJest.js'],
-    testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-    transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
+    projects: [
+        {
+            displayName: 'Unit Tests',
+            roots: ['<rootDir>/src'],
+            testPathIgnorePatterns: ['/node_modules/', '/src/integration'],
+            setupFiles: ['./setupJestUnit.js'],
+
+            testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+            transform: {
+                '^.+\\.(ts|tsx)$': 'ts-jest',
+            },
+        },
+        {
+            displayName: 'Integration Tests',
+            roots: ['<rootDir>/src/integration'],
+            setupFiles: ['./setupJestIntegration.js'],
+
+            testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+            transform: {
+                '^.+\\.(ts|tsx)$': 'ts-jest',
+            },
+        },
+    ],
 };
