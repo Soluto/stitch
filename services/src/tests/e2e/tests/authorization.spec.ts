db43d8f22516f74ae1869ec1771ea8747c48f3c4
Tomer Eskenazi <tomeresk@gmail.com>
2020-06-28 18:27:33 +0300

    allow jwt in param injection (policy authorization can use it through args)

--- services/src/tests/e2e/tests/authorization.spec.ts
+++ services/src/tests/e2e/tests/authorization.spec.ts
@@ -6,16 +6,21 @@ import {
     createSchemaMutation,
     createPolicyMutation,
     onlyAdminPolicy,
+    jwtNamePolicy,
+    getArbitraryDataQuery,
 } from '../../helpers/authzSchema';
 
-const gatewayClient = new GraphQLClient('http://localhost:8080/graphql');
-const registryClient = new GraphQLClient('http://localhost:8090/graphql');
+const gatewayClient = createGatewayClient();
+const registryClient = createRegistryClient();
 
 describe('authorization', () => {
     // This is kind of both the "before" section and a test, but it was weird putting a test in an actual before section
     it('creates the policy and schema resources', async () => {
-        const policyResponse = await registryClient.request(createPolicyMutation, {policy: onlyAdminPolicy()});
-        expect(policyResponse.updatePolicies.success).toBe(true);
+        const policy1Response = await registryClient.request(createPolicyMutation, {policy: onlyAdminPolicy()});
+        expect(policy1Response.updatePolicies.success).toBe(true);
+
+        const policy2Response = await registryClient.request(createPolicyMutation, {policy: jwtNamePolicy()});
+        expect(policy2Response.updatePolicies.success).toBe(true);
 
         const schemaResponse = await registryClient.request(createSchemaMutation, {schema: getSchema()});
         expect(schemaResponse.updateSchemas.success).toBe(true);
@@ -42,4 +47,61 @@ describe('authorization', () => {
         expect(response.errors[0].path).toEqual(['user', 'lastName']);
         expect(response.data.user).toEqual({firstName: 'John', lastName: null, role: 'normal'});
     });
+
+    it('rejects access to a field based on JWT info when no JWT is sent', async () => {
+        let response;
+        try {
+            await gatewayClient.request(getArbitraryDataQuery());
+        } catch (err) {
+            response = err.response;
+        }
+
+        expect(response.errors).toHaveLength(1);
+        expect(response.errors[0].message).toBe('Unauthorized by policy jwtName in namespace ns');
+        expect(response.errors[0].path).toEqual(['arbitraryData', 'arbitraryField']);
+        expect(response.data.arbitraryData).toEqual({arbitraryField: null});
+    });
+
+    it('rejects access to a field based on JWT info', async () => {
+        const gatewayClientJwt = createGatewayClient(disallowedJwtOptions());
+        let response;
+        try {
+            await gatewayClientJwt.request(getArbitraryDataQuery());
+        } catch (err) {
+            response = err.response;
+        }
+
+        expect(response.errors).toHaveLength(1);
+        expect(response.errors[0].message).toBe('Unauthorized by policy jwtName in namespace ns');
+        expect(response.errors[0].path).toEqual(['arbitraryData', 'arbitraryField']);
+        expect(response.data.arbitraryData).toEqual({arbitraryField: null});
+    });
+
+    it('allows access to a field based on JWT info', async () => {
+        const gatewayClientJwt = createGatewayClient(allowedJwtOptions());
+        const response = await gatewayClientJwt.request(getArbitraryDataQuery());
+        expect(response.arbitraryData).toEqual({arbitraryField: 'arbitraryValue'});
+    });
 });
+
+function allowedJwtOptions() {
+    // Claims: { "name": "Varg", "sub": "1234567890", "iat": 1516239022 }
+    const encodedJwt =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlZhcmciLCJpYXQiOjE1MTYyMzkwMjJ9.MEXFHfBaci44tuGREvCK1vZ-KqYL7L-TYqO7v0jfsYM';
+    return {headers: {authorization: `Bearer ${encodedJwt}`}};
+}
+
+function disallowedJwtOptions() {
+    // Claims: { "name": "Orm", "sub": "1234567890", "iat": 1516239022 }
+    const encodedJwt =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik9ybSIsImlhdCI6MTUxNjIzOTAyMn0.ZQ__h2OJR8A7uSKTZ3QC0jP5P3U3a1JuMU9xVnzWTIM';
+    return {headers: {authorization: `Bearer ${encodedJwt}`}};
+}
+
+function createRegistryClient(options = {}) {
+    return new GraphQLClient('http://localhost:8090/graphql', options);
+}
+
+function createGatewayClient(options = {}) {
+    return new GraphQLClient('http://localhost:8080/graphql', options);
+}
