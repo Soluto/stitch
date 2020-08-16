7a6ca76b0031a86ad0264f82a365c3bde3c75261
AleF83 <AlexanderFux83@gmail.com>
2020-08-16 19:47:30 +0300

    Arguments injection - return template as is on evaluation fail

--- services/src/modules/arguments-injection/arguments-evaluation.ts
+++ services/src/modules/arguments-injection/arguments-evaluation.ts
@@ -4,14 +4,18 @@ const exprStart = /{/g;
 const fullCapture = /(^{[^{]+}$)|(^{{.+}}$)/g;
 
 function evaluate<T>(template: string, data?: Record<string, unknown>): T {
-  const vm = new VM().setGlobals(data);
-  const fullMatch = template.match(fullCapture);
-  if (fullMatch) {
-    return vm.run(`(function evaluate() { return ${template.slice(1, -1)}; })()`);
-  }
+  try {
+    const vm = new VM().setGlobals(data);
+    const fullMatch = template.match(fullCapture);
+    if (fullMatch) {
+      return vm.run(`(function evaluate() { return ${template.slice(1, -1)}; })()`);
+    }
 
-  const expression = template.replace(exprStart, '${');
-  return vm.run(`\`${expression}\``);
+    const expression = template.replace(exprStart, '${');
+    return vm.run(`\`${expression}\``);
+  } catch {
+    return (template as unknown) as T;
+  }
 }
 
 export default evaluate;
