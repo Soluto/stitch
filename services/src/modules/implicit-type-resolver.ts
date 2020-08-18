e3b319999f7cd78e435f52ebb58fb73f583bcb6b
Alex <AlexanderFux83@gmail.com>
2020-08-18 21:05:13 +0300

    Implicit type resolver (#177)
    * Tests added - fails without implementation
    
    * Resolve interface and union types implicitly

---
+++ services/src/modules/implicit-type-resolver.ts
@@ -0,0 +1,32 @@
+import { visit, DocumentNode, GraphQLError } from 'graphql';
+import { IResolvers } from 'graphql-tools';
+
+type SourceWithTypename = {
+  __typename: string;
+};
+
+function createTypeResolver(name: string) {
+  return {
+    __resolveType(obj: SourceWithTypename) {
+      if (!obj.__typename) {
+        throw new GraphQLError(
+          `Expecting a "__typename" property in source when resolving an interface or union: ${name}.`
+        );
+      }
+      return obj.__typename;
+    },
+  };
+}
+
+export default function createTypeResolvers(schema: DocumentNode) {
+  const resolvers: IResolvers<SourceWithTypename> = {};
+  visit(schema, {
+    InterfaceTypeDefinition(node) {
+      resolvers[node.name.value] = createTypeResolver(node.name.value);
+    },
+    UnionTypeDefinition(node) {
+      resolvers[node.name.value] = createTypeResolver(node.name.value);
+    },
+  });
+  return resolvers;
+}
