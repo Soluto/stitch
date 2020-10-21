c66204dbd5d5a36e84604ce9577aab39cdb97db4
Alex <AlexanderFux83@gmail.com>
2020-10-21 22:42:54 +0300

    Log 401 errors on debug level instead of warn (#194)

--- services/src/modules/authentication/strategies/jwt.ts
+++ services/src/modules/authentication/strategies/jwt.ts
@@ -8,14 +8,14 @@ export default async function (request: fastify.FastifyRequest): Promise<void> {
   const issuer = String(decodedJWT.payload.iss);
   const issuerConfig = authenticationConfig?.jwt?.[issuer];
   if (!issuerConfig) {
-    logger.warn({ issuer }, 'Unknown issuer');
+    logger.debug({ issuer }, 'Unknown issuer');
     throw new Error('Unauthorized');
   }
-  if (issuerConfig.authenticatedPaths.some(ap => request.raw.url?.endsWith(ap))) {
+  if (issuerConfig.authenticatedPaths.some(ap => request.raw.url === ap)) {
     try {
       await request.jwtVerify();
     } catch (error) {
-      logger.warn(error, 'Failed to verify request JWT');
+      logger.debug(error, 'Failed to verify request JWT');
       throw new Error('Unauthorized');
     }
   }
