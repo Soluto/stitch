19c6f6dfda7d18c3b46ca499f9c476ddf77b4d93
AleF83 <AlexanderFux83@gmail.com>
2020-11-29 12:37:37 +0200

    Exclude resource types

--- cli/tests/apply-resources.spec.ts
+++ cli/tests/apply-resources.spec.ts
@@ -12,10 +12,17 @@ describe('Apply resources', () => {
       })
     )
     .stdout()
-    .command(['apply:resources', '--dry-run', '--registry-url=http://registry/graphql', 'tests/resources'])
+    .command([
+      'apply:resources',
+      '--dry-run',
+      '--registry-url=http://registry/graphql',
+      '--exclude=upstreams',
+      'tests/resources',
+    ])
     .it('Verify', ctx => {
       expect(ctx.stdout).to.contain('schemas: 1');
       expect(ctx.stdout).to.contain('policies: 1');
+      expect(ctx.stdout).to.contain('upstreams: 1 - Skipped');
       expect(ctx.stdout).to.contain('were verified successfully');
     });
 });
