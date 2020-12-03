a67fdaad3eea57919e01f8eeac8a4b8c1eb7a828
AleF83 <AlexanderFux83@gmail.com>
2020-12-03 08:38:34 +0200

    Glob package fixed

--- cli/src/utils/build-stitched-schema.ts
+++ cli/src/utils/build-stitched-schema.ts
@@ -1,5 +1,5 @@
 import { promises as fs } from 'fs';
-import * as fg from 'fast-glob';
+import * as globby from 'globby';
 import { ResourceMetadataInput, SchemaInput } from '../client';
 
 interface SchemaV2 {
@@ -8,7 +8,7 @@ interface SchemaV2 {
 }
 
 export default async function ({ metadata, schemaFiles }: SchemaV2): Promise<SchemaInput> {
-  const files = await fg(schemaFiles);
+  const files = await globby(schemaFiles);
   const fileContents = await Promise.all(files.map(f => fs.readFile(f, { encoding: 'utf8' })));
   const schema = fileContents.join('\n\n');
   return {
