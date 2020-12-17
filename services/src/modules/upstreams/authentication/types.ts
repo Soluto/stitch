1fab338229ba3fe1efc6e33585ae5bb6fde3aabc
AleF83 <AlexanderFux83@gmail.com>
2020-12-17 22:52:48 +0200

    Remove unused types

--- services/src/modules/upstreams/authentication/types.ts
+++
@@ -1,8 +0,0 @@
-import { Upstream, UpstreamClientCredentials } from '../../resource-repository';
-import { ActiveDirectoryAuth } from '.';
-
-export interface AuthenticationConfig {
-  getUpstreamByHost: (host: string) => Upstream | undefined;
-  getUpstreamClientCredentialsByAuthority: (authority: string) => UpstreamClientCredentials | undefined;
-  activeDirectoryAuth: ActiveDirectoryAuth;
-}
