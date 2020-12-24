c55b1058b2276b01e8705eba09cb6879d7e946db
AleF83 <AlexanderFux83@gmail.com>
2020-12-24 09:31:46 +0200

    Add wrong schema update e2e test

--- services/tests/e2e/basic/hello-world.spec.ts
+++ services/tests/e2e/basic/hello-world.spec.ts
@@ -1,6 +1,7 @@
 import { print } from 'graphql';
 import { gql } from 'apollo-server-core';
 import { GraphQLClient } from 'graphql-request';
+import GraphQLErrorSerializer from '../../utils/graphql-error-serializer';
 import { sleep } from '../../helpers/utility';
 import {
   createSchemaMutation,
@@ -8,7 +9,7 @@ import {
   emptySchema,
 } from '../../helpers/registry-request-builder';
 import { getToken } from '../../helpers/get-token';
-import { schema1, schema2 } from './hello-world.schema';
+import { schema1, schema2, schema3 } from './hello-world.schema';
 
 const gatewayClient = new GraphQLClient('http://localhost:8080/graphql');
 const registryClient = new GraphQLClient('http://localhost:8090/graphql');
@@ -21,6 +22,8 @@ describe('Basic flow', () => {
   `);
 
   beforeAll(async () => {
+    expect.addSnapshotSerializer(GraphQLErrorSerializer);
+
     const accessToken = await getToken();
     gatewayClient.setHeader('Authorization', `Bearer ${accessToken}`);
   });
@@ -49,15 +52,25 @@ describe('Basic flow', () => {
   });
 
   test('Gateway updates when updating schema in registry', async () => {
-    const response1: UpdateSchemasMutationResponse = await registryClient.request(createSchemaMutation, {
+    const response2: UpdateSchemasMutationResponse = await registryClient.request(createSchemaMutation, {
       schema: schema2,
     });
-    expect(response1.updateSchemas.success).toBeTruthy();
+    expect(response2.updateSchemas.success).toBeTruthy();
 
     // Wait for gateway to update
     await sleep(Number(process.env.WAIT_FOR_REFRESH_ON_GATEWAY) | 1500);
 
     const response = await gatewayClient.request(query);
     expect(response).toMatchSnapshot();
+  });
+
+  test('Send wrong schema', async () => {
+    try {
+      await registryClient.request(createSchemaMutation, {
+        schema: schema3,
+      });
+    } catch (e) {
+      expect(e.response).toMatchSnapshot();
+    }
   });
 });
