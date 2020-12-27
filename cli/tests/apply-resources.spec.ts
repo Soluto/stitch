e090a734d873a23dfe58b446198ac2d7aa3ab3da
Alex <AlexanderFux83@gmail.com>
2020-12-27 14:53:06 +0200

    CLI: Merge type extensions (#244)

--- cli/tests/apply-resources.spec.ts
+++ cli/tests/apply-resources.spec.ts
@@ -1,47 +1,67 @@
-/* eslint-disable promise/valid-params */
 /* eslint-disable promise/catch-or-return */
-import { expect, test } from '@oclif/test';
+import { promises as fs, readFileSync } from 'fs';
+import { join } from 'path';
+import { expect, test, FancyTypes } from '@oclif/test';
 
-describe('Apply resources', () => {
-  test
-    .nock('http://registry', api =>
-      api.post('/graphql').reply(200, {
-        data: {
-          validateResourceGroup: {
-            success: true,
-          },
+const commandArgs = (testCaseName: string) => [
+  'apply:resources',
+  '--dry-run',
+  '--registry-url=http://registry/graphql',
+  '--skip-resource-types=upstreams,upstreamClientCredentials',
+  join(__dirname, './data/apply-resources', testCaseName),
+];
+
+const nockCb = (testCaseName: string) => (api: FancyTypes.NockScope) =>
+  api
+    .post('/graphql', body => {
+      const receivedSchema = body?.variables?.resourceGroup?.schemas?.[0]?.schema as string;
+      const requestFile = join(__dirname, './requests/apply-resources', `${testCaseName}.gql`);
+      const expectedSchema = readFileSync(requestFile, { encoding: 'utf8' });
+      return receivedSchema === expectedSchema;
+    })
+    .reply(200, {
+      data: {
+        validateResourceGroup: {
+          success: true,
         },
-      })
-    )
-    .stdout()
-    .command([
-      'apply:resources',
-      '--dry-run',
-      '--registry-url=http://registry/graphql',
-      '--skip-resource-types=upstreams,upstreamClientCredentials',
-      'tests/resources',
-    ])
-    .it('Verify', ctx => {
-      expect(ctx.stdout).to.contain('schemas: 2');
-      expect(ctx.stdout).to.contain('policies: 1');
-      expect(ctx.stdout).to.contain('upstreams: 1 - Skipped');
-      expect(ctx.stdout).to.contain('upstreamClientCredentials: 0 - Skipped');
-      expect(ctx.stdout).to.contain('were verified successfully');
+      },
     });
 
-  test
-    .nock('http://registry', api => api.post('/graphql').delay(100).reply(200))
-    .stdout()
-    .stderr()
-    .command([
-      'apply:resources',
-      '--dry-run',
-      '--registry-url=http://registry/graphql',
-      '--timeout=50',
-      'tests/resources',
-    ])
-    .catch((e: Error) => expect(e.message).contains('network timeout at: http://registry/graphql'), {
-      raiseIfNotThrown: true,
-    })
-    .it('Timeout', () => {});
+async function runTestCase(testCaseName: string) {
+  if (testCaseName.includes('error')) {
+    describe(testCaseName, () => {
+      test
+        .stdout()
+        .command(commandArgs(testCaseName))
+        .catch('EEXIT: 1')
+        .end('run command with error', ctx => {
+          ctx.stdout.includes('Verifying resources failed');
+        });
+    });
+  } else {
+    describe(testCaseName, () => {
+      test
+        .nock('http://registry', nockCb(testCaseName))
+        .stdout()
+        .command(commandArgs(testCaseName))
+        .end('run command', ctx => {
+          ctx.stdout.includes('were verified successfully.');
+          ctx.stdout.includes('schemas: 1');
+          ctx.stdout.includes('upstreamClientCredentials: 0 - Skipped');
+        });
+    });
+  }
+}
+
+describe('Apply resources', () => {
+  let testCases: string[];
+
+  before(async () => {
+    testCases = await fs.readdir(join(__dirname, './data/apply-resources'));
+    testCases.forEach(runTestCase);
+  });
+
+  it("Workaround - mocha doesn't support async describe", () => {
+    expect(0).to.eq(0);
+  });
 });
