1637f8f87f426d41e3bb176397473b99cfb3137b
Alex <AlexanderFux83@gmail.com>
2021-01-18 20:21:21 +0200

    More logs for registry (#261)

--- services/src/modules/registry-schema/resolvers/delete-resources.ts
+++ services/src/modules/registry-schema/resolvers/delete-resources.ts
@@ -19,39 +19,52 @@ export default async function (deletions: ResourceGroupMetadataInput, context: R
     const policyAttachments = new PolicyAttachmentsHelper();
 
     try {
+      logger.trace('Fetching latest resource group...');
       const { resourceGroup } = await resourceRepository.fetchLatest();
       const existingPolicies = _.cloneDeep(resourceGroup.policies);
 
+      logger.trace('Applying resource group updates...');
       const newRg = applyResourceGroupDeletions(resourceGroup, deletions);
 
       // TODO: In case of upstream deletion we should force refresh of remote schemas
+      logger.trace('Updating remote gql schemas...');
       updateRemoteGqlSchemas(newRg, context);
 
       const registryRg = _.cloneDeep(newRg);
 
+      logger.trace('Applying plugins for resource group...');
       const gatewayRg = await applyPluginForResourceGroup(newRg);
+
+      logger.trace('Validating resource group...');
       validateResourceGroupOrThrow(gatewayRg);
+
+      logger.trace('Creating schema config...');
       await createSchemaConfig(gatewayRg);
 
+      logger.trace('Synchronizing policy attachments...');
       await policyAttachments.sync(existingPolicies, gatewayRg.policies);
 
+      logger.trace('Saving policy attachments...');
       await policyAttachments.writeToRepo();
+
+      logger.trace('Saving resource group...');
       await Promise.all([
         resourceRepository.update(registryRg, { registry: true }),
         resourceRepository.update(gatewayRg),
       ]);
-
-      const summary = Object.fromEntries(
-        Object.entries(deletions).map(([k, v]) => [k, v ? (Array.isArray(v) ? v.length : 1) : 0])
-      );
-      logger.info(summary, `Resources were deleted`);
     } catch (err) {
       const message = `Resources deletion request failed: ${err}`;
       logger.error({ err }, message);
       throw new GraphQLError(message);
     } finally {
+      logger.trace('Removing policy attachments temporary files...');
       await policyAttachments.cleanup();
     }
+
+    const summary = Object.fromEntries(
+      Object.entries(deletions).map(([k, v]) => [k, v ? (Array.isArray(v) ? v.length : 1) : 0])
+    );
+    logger.info(summary, `Resources were deleted`);
 
     return { success: true };
   });
--- services/src/modules/registry-schema/resolvers/update-resource-group.ts
+++ services/src/modules/registry-schema/resolvers/update-resource-group.ts
@@ -4,7 +4,7 @@ import logger from '../../logger';
 import { createSchemaConfig } from '../../graphql-service';
 import { applyResourceGroupUpdates } from '../../resource-repository';
 import { validateResourceGroupOrThrow } from '../validation';
-import { transformResourceGroup as applyPluginForResourceGroup } from '../../plugins';
+import { transformResourceGroup as applyPluginsForResourceGroup } from '../../plugins';
 import { PolicyAttachmentsHelper, markOptionalPolicyArgs } from '../helpers';
 import resourceRepository from '../repository';
 import { ResourceGroupInput } from '../types';
@@ -19,43 +19,56 @@ export default async function (updates: ResourceGroupInput, context: RegistryReq
     const policyAttachments = new PolicyAttachmentsHelper();
 
     try {
+      logger.trace('Fetching latest resource group...');
       const { resourceGroup } = await resourceRepository.fetchLatest();
       const existingPolicies = _.cloneDeep(resourceGroup.policies);
 
+      logger.trace('Applying resource group updates...');
       const newRg = applyResourceGroupUpdates(resourceGroup, updates);
 
       // TODO: In case of upstream deletion we should force refresh of remote schemas
+      logger.trace('Updating remote gql schemas...');
       await updateRemoteGqlSchemas(newRg, context);
 
       const registryRg = _.cloneDeep(newRg);
 
-      const gatewayRg = await applyPluginForResourceGroup(newRg);
+      logger.trace('Applying plugins for resource group...');
+      const gatewayRg = await applyPluginsForResourceGroup(newRg);
+
+      logger.trace('Validating resource group...');
       validateResourceGroupOrThrow(gatewayRg);
+
+      logger.trace('Creating schema config...');
       await createSchemaConfig(gatewayRg);
 
+      logger.trace('Marking optional policy arguments...');
       markOptionalPolicyArgs(updates.policies);
+
+      logger.trace('Synchronizing policy attachments...');
       await policyAttachments.sync(existingPolicies, gatewayRg.policies);
 
       if (!dryRun) {
+        logger.trace('Saving policy attachments...');
         await policyAttachments.writeToRepo();
+        logger.trace('Saving resource group...');
         await Promise.all([
           resourceRepository.update(registryRg, { registry: true }),
           resourceRepository.update(gatewayRg),
         ]);
       }
-
-      const summary = Object.fromEntries(
-        Object.entries(updates).map(([k, v]) => [k, v ? (Array.isArray(v) ? v.length : 1) : 0])
-      );
-      logger.info(summary, `Resources were ${dryRun ? 'validated' : 'updated'}`);
     } catch (err) {
       const message = `${dryRun ? 'Validate' : 'Updated'} resources request failed: ${err}`;
       logger.error({ err }, message);
       throw err;
     } finally {
+      logger.trace('Removing policy attachments temporary files...');
       await policyAttachments.cleanup();
     }
 
+    const summary = Object.fromEntries(
+      Object.entries(updates).map(([k, v]) => [k, v ? (Array.isArray(v) ? v.length : 1) : 0])
+    );
+    logger.info(summary, `Resources were ${dryRun ? 'validated' : 'updated'}`);
     return { success: true };
   });
 }
