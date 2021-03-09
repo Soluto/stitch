3577346b1a55b407dc087bd36da93fca8d734531
AleF83 <alexanderfux83@gmail.com>
2021-03-09 17:56:00 +0200

    Fix existing tests

--- services/tests/integration/gateway/rest-directive/directive.spec.ts
+++ services/tests/integration/gateway/rest-directive/directive.spec.ts
@@ -307,7 +307,7 @@ describe.each(testCases)('Rest directive', (testName, { mock, schema, query, var
 
     const { server, dispose } = createStitchGateway({
       resourceGroups: Rx.of(resourceGroup),
-      fastifyInstance: { metrics: undefined as any },
+      fastifyInstance: { metrics: undefined as any } as any,
     });
     client = createTestClient(server);
     disposeServer = dispose;
