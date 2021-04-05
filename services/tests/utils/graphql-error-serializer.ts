010560660bc4a37aa9c98f7aea6c1cdf6d2b502e
AleF83 <alexanderfux83@gmail.com>
2021-04-05 12:38:09 +0300

    @apollo/federation: update version & fix breaking changes

--- services/tests/utils/graphql-error-serializer.ts
+++ services/tests/utils/graphql-error-serializer.ts
@@ -1,17 +1,29 @@
 import { GraphQLError } from 'graphql';
 
+interface SerializedError {
+  name: string;
+  message: string;
+  errorCode?: string;
+  innerErrors?: string[];
+}
+
 export default {
   test(arg: any): boolean {
     return arg && Object.prototype.hasOwnProperty.call(arg, 'errors') && arg.errors && arg.errors.length > 0;
   },
   print(val: any): string {
-    const errors = (val.errors as GraphQLError[]).map(e => ({
-      name: e.name,
-      message: e.message,
-      exception: {
-        code: e.extensions?.code,
-      },
-    }));
+    const errors = (val.errors as GraphQLError[]).map(e => {
+      const error: SerializedError = {
+        name: e.name,
+        message: e.message,
+        errorCode: e.extensions?.code,
+      };
+      if (e.extensions?.errors) {
+        error.innerErrors = e.extensions?.errors?.map((err: Error) => err.message);
+      }
+      return error;
+    });
+
     const data = val.data ?? {};
 
     return JSON.stringify({ errors, data }, undefined, 2);
