6ff70341c3ad0860a1eb0fdeabe88d85c5de9103
AleF83 <alexanderfux83@gmail.com>
2021-05-02 13:42:04 +0300

    @policies directive on object tests added

--- services/tests/e2e/authorization/auth-policies-directive.schema.ts
+++ services/tests/e2e/authorization/auth-policies-directive.schema.ts
@@ -12,7 +12,7 @@ export const policies = [
       }
     `,
     args: {
-      argA: { type: 'String', default: '{args.argA}' },
+      argA: { type: 'String', default: '{args.argA || source.argA}' },
     },
   },
   {
@@ -25,7 +25,7 @@ export const policies = [
       }
     `,
     args: {
-      argB: { type: 'String', default: '{args.argB}' },
+      argB: { type: 'String', default: '{args.argB || source.argB}' },
     },
   },
 ];
@@ -34,18 +34,33 @@ export const schema = {
   metadata: { namespace: 'auth_policies', name: 'policies_directive' },
   schema: print(gql`
     type Query {
-      pd_foo(argA: String!, argB: String!): String!
+      pd_foo(argA: String!, argB: String!): String
         @stub(value: "FOO")
         @policies(
           policies: [{ namespace: "auth_policies", name: "isAlpha" }, { namespace: "auth_policies", name: "isBeta" }]
           relation: OR
         )
-      pd_bar(argA: String!, argB: String!): String!
+      pd_bar(argA: String!, argB: String!): String
         @stub(value: "FOO")
         @policies(
           policies: [{ namespace: "auth_policies", name: "isAlpha" }, { namespace: "auth_policies", name: "isBeta" }]
           relation: AND
         )
+      pd_baz(argA: String!, argB: String!): PDBaz
+        @localResolver(
+          value: """
+          {{
+            argA: args.argA, argB: args.argB
+          }}
+          """
+        )
+    }
+
+    type PDBaz
+      @policies(
+        policies: [{ namespace: "auth_policies", name: "isAlpha" }, { namespace: "auth_policies", name: "isBeta" }]
+      ) {
+      bazbaz: String @localResolver(value: "BAZBAZ")
     }
   `),
 };
@@ -54,5 +69,8 @@ export const query = print(gql`
   query PoliciesDirective($argA: String!, $argB: String!) {
     pd_foo(argA: $argA, argB: $argB)
     pd_bar(argA: $argA, argB: $argB)
+    pd_baz(argA: $argA, argB: $argB) {
+      bazbaz
+    }
   }
 `);
